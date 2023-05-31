2c172ed01fe44f391d5354a181fd227cca7036c5
chandansgowda <chandansuresh007@gmail.com>
2023-05-31 15:54:46 +0530

    feat: firebase setup

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+import cors from "cors";
+import { db } from "./firebase.js";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const PORT = 3000;
 
-app.listen(port, () => {
-  console.log(`Resonate Backend listening on port ${port}`)
-})
+const app = express();
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Resonate Backend listening on port ${PORT}`);
+});

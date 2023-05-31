f0af188cf6d07524bd7adc8e989e5debe0ce1f49
simardeep <smudhar_be21@thapar.edu>
2023-05-31 18:40:16 +0530

    feat : added token-generation and room-creation

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+// import { db } from "./firebase.js";
+// import { collection, getDocs, addDoc } from "firebase/firestore";
+
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+//importing routes
+import {roomRouter} from './routes/Room.js'
+import { tokenRouter } from "./routes/Token.js";
+
+//Connecting Endpoints to imported routes
+app.use("/Room",roomRouter);
+app.use('/Token',tokenRouter);
+
+app.listen(PORT, () => {
+  console.log(`Resonate Backend listening on port ${PORT}`);
+});
---
+++ controllers/CreateRoom.js
@@ -0,0 +1,33 @@
+//livekit-server-sdk imports
+import { RoomServiceClient} from 'livekit-server-sdk';
+const livekitHost = 'https://my.livekit.host';
+const svc = new RoomServiceClient(livekitHost, `${process.env.LIVEKIT_API_KEY}`, `${process.env.LIVEKIT_API_SECRET}`);
+
+const createRoom = (req,res)=> {
+    console.log('Requested Room Data:',req.body);
+    try{
+        // list rooms
+        svc.listRooms().then((rooms) => {
+            console.log('existing rooms', rooms);
+        });
+        
+        // create a new room
+        const roomName = req.body.roomName;
+        const opts = {
+            name: roomName,
+            // timeout in seconds
+            emptyTimeout: 10 * 60,
+            maxParticipants: 20,
+        };
+        svc.createRoom(opts).then((room) => {
+            console.log('room created', room);
+            res.json({"msg":"Room created Successfully","room":room});
+        });   
+    }catch(error){
+        console.log(error);
+        res.json({"msg":"Error creating room"});
+    }
+    }
+
+
+export {createRoom};

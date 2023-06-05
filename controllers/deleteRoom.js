6886e4d45aa58a7727cfd257ea5c0c095ddcc6ee
simardeep <smudhar_be21@thapar.edu>
2023-06-05 20:22:38 +0530

    added delete-room controller

---
+++ controllers/deleteRoom.js
@@ -0,0 +1,19 @@
+//livekit-server-sdk imports
+import { RoomServiceClient} from 'livekit-server-sdk';
+const livekitHost = 'https://my.livekit.host';
+const svc = new RoomServiceClient(livekitHost, `${process.env.LIVEKIT_API_KEY}`, `${process.env.LIVEKIT_API_SECRET}`);
+
+const deleteRoom = (req,res) => {
+    console.log('Deleting room with requested Data:',req.body);
+    const roomName = req.body.roomName;
+    try{
+        svc.deleteRoom(roomName).then(() => {
+            console.log('Room deleted:',roomName);
+          });
+    }catch(e){
+        console.log('Error occured while deleting Room :',e);
+        res.json({"msg":"Error deleting room"});
+    }
+}
+
+export {deleteRoom};
--- routes/room.js
+++ routes/room.js
@@ -3,7 +3,9 @@ const roomRouter = express.Router();
 
 //importing Controllers
 import {createRoom} from '../controllers/createRoom.js'
+import {deleteRoom} from '../controllers/deleteRoom.js';
 
 roomRouter.route('/createRoom').post(createRoom);
+roomRouter.route('/deleteRoom').post(deleteRoom);
 
 export {roomRouter};

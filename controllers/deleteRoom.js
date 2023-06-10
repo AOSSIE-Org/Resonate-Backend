a20faaffe5d981d7ff48e3108bc6c058548d55f6
chandansgowda <chandansuresh007@gmail.com>
2023-06-10 14:31:22 +0530

    feat: verify token + delete room data on firebase

--- controllers/deleteRoom.js
+++ controllers/deleteRoom.js
@@ -1,22 +1,44 @@
-//livekit-server-sdk imports
-import { RoomServiceClient } from "livekit-server-sdk";
-const livekitHost = "https://my.livekit.host";
+import { RoomServiceClient, TokenVerifier } from "livekit-server-sdk";
+import { db } from "../firebase.js";
+import { doc, deleteDoc } from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
+
 const svc = new RoomServiceClient(
-  livekitHost,
+  `${process.env.LIVEKIT_HOST}`,
   `${process.env.LIVEKIT_API_KEY}`,
   `${process.env.LIVEKIT_API_SECRET}`
 );
 
-const deleteRoom = (req, res) => {
+const tokenVerifier = new TokenVerifier(
+  `${process.env.LIVEKIT_API_KEY}`,
+  `${process.env.LIVEKIT_API_SECRET}`
+);
+
+const deleteRoom = async (req, res) =>{
   console.log("Deleting room with requested Data:", req.body);
-  const roomName = req.body.roomName;
+  const firebaseRoomDocId = req.body.firebaseRoomDocId;
+  const token = req.body.token;
   try {
-    svc.deleteRoom(roomName).then(() => {
-      console.log("Room deleted:", roomName);
-    });
+    const isTokenValid = tokenVerifier.verify(token);
+    if (
+      isTokenValid.video.room === firebaseRoomDocId &&
+      isTokenValid.video.roomCreate === true
+    ) {
+
+      //Delete Firebase room doc
+      await deleteDoc(doc(db, "rooms", firebaseRoomDocId));
+      console.log("Firebase Room Doc deleted");
+
+      // Delete livekit room
+      svc.deleteRoom(firebaseRoomDocId).then(() => {
+        console.log("Livekit Room deleted:", firebaseRoomDocId);
+        res.json({ msg: "Success" });
+      });
+    }
   } catch (e) {
     console.log("Error occured while deleting Room :", e);
-    res.json({ msg: "Error deleting room" });
+    res.status(400).json({ msg: "Invalid Token" });
   }
 };
 

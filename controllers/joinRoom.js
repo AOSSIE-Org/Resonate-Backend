0452d4628f0e9fbf326c020898d3c9fe7eebcb4a
chandansgowda <chandansuresh007@gmail.com>
2023-06-11 21:00:16 +0530

    feat(#18): join room endpoint

--- controllers/joinRoom.js
+++ controllers/joinRoom.js
@@ -1,6 +1,21 @@
+import { generateToken } from "./generateToken.js";
+
 const joinRoom = async (req, res) => {
+    //TODO: verify if the user with same username has requested for a token.
+  try {
     console.log("Request Data: ", req.body);
-    res.end("Working")
-}
+    const roomName = req.body.room_name;
+    const username = req.body.username;
+    // Creating a token for the user
+    const token = generateToken(roomName, username, false);
+    res.json({
+      msg: "Success",
+      access_token: token,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ msg: "Error" });
+  }
+};
 
 export { joinRoom };
